8f6c5c69d7f0e19991da9e12deb85ebdc3e862ac
Alexandra Serebrennikova <sasha.serebrennik@gmail.com>
2016-02-16 23:50:45 +0300

    dz1 done

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,22 +2,26 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
 
-		shell: {
-			server: {
-				command: 'node server.js'			
-			}
-		},
+        shell: {
+            server: {
+                command: 'node server.js'            
+            }
+            // запуск сервера через скрипт shell'a https://www.npmjs.com/package/grunt-shell
+        },
 
-		watch: {
+        watch: {
             files: ['templates/*.xml'],
             tasks: ['fest']
-		},
-		
-		concurrent: {
+            // запуск watcher'a, который следит за изенениями файлов  templates/*.xml
+            // и если они изменяются, то запускает таск сборки шаблонов (grunt fest)
+        },
+        
+        concurrent: {
             tasks: ['shell', 'watch']
-		},
+            // одновременный запуска shell'a и watcher'a https://www.npmjs.com/package/grunt-concurrent
+        },
 
-		fest: {
+        fest: {
             templates: {
                 files: [{
                     expand: true,
@@ -38,11 +42,11 @@ module.exports = function (grunt) {
 
     });
 
-	// подключть все необходимые модули
+    // подключть все необходимые модули
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-concurrent');
-	grunt.loadNpmTasks('grunt-shell');
-	grunt.loadNpmTasks('grunt-fest');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-fest');
 
     // результат команды grunt
     grunt.registerTask('default', ['shell', 'watch']);
